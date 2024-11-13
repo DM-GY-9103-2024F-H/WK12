453b42ff7da0cca98a77c0d2a82fcb59bccd4301
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-11-13 04:39:18 -0500

    Add cnn loader capability

--- WKI_utils.py
+++ WKI_utils.py
@@ -8,17 +8,19 @@ from data_utils import LFWUtils as LFWUtils_Linear
 
 class FaceDataset(Dataset):
   @staticmethod
-  def toTensor(pxs):
-    return pxs.reshape(-1, LFWUtils.IMAGE_SIZE[1], LFWUtils.IMAGE_SIZE[0])
+  def toTensor(pxs, norm=1):
+    t = pxs.reshape(-1, LFWUtils.IMAGE_SIZE[1], LFWUtils.IMAGE_SIZE[0])
+    return t / norm
 
   @staticmethod
   def toPixels(img):
     return img.reshape(-1)
 
-  def __init__(self, imgs, labels, transform=None):
+  def __init__(self, imgs, labels, transform=None, cnn_loader=False):
     self.imgs = imgs
     self.labels = labels
     self.transform = transform
+    self.cnn = cnn_loader
 
   def __len__(self):
     return len(self.labels)
@@ -26,24 +28,31 @@ class FaceDataset(Dataset):
   def __getitem__(self, idx):
     img, label = self.imgs[idx], self.labels[idx]
     if self.transform and torch.is_grad_enabled():
-      img = FaceDataset.toPixels(self.transform(FaceDataset.toTensor(img)))
+      if self.cnn:
+        img = FaceDataset.toTensor(img, norm=255)
+        img = self.transform(img)
+      else:
+        img = FaceDataset.toTensor(img, norm=1)
+        img = FaceDataset.toPixels(self.transform(img))
+    elif self.cnn:
+      img = FaceDataset.toTensor(img, norm=255)
     return img, label
 
 
 class LFWUtils(LFWUtils_Linear):
   @staticmethod
-  def train_test_split(test_pct=0.5, random_state=101010, return_loader=True, train_transform=None, test_transform=None):
+  def train_test_split(test_pct=0.5, random_state=101010, return_loader=True, cnn_loader=False, train_transform=None, test_transform=None):
     train, test = LFWUtils_Linear.train_test_split(test_pct=test_pct, random_state=random_state)
 
-    if return_loader:
+    if return_loader or cnn_loader:
       x_train = Tensor(train["pixels"])
       y_train = Tensor(train["labels"]).long()
 
       x_test = Tensor(test["pixels"])
       y_test = Tensor(test["labels"]).long()
 
-      train_dataloader = DataLoader(FaceDataset(x_train, y_train, train_transform), batch_size=256, shuffle=True)
-      test_dataloader = DataLoader(FaceDataset(x_test, y_test, test_transform), batch_size=512)
+      train_dataloader = DataLoader(FaceDataset(x_train, y_train, train_transform, cnn_loader), batch_size=256, shuffle=True)
+      test_dataloader = DataLoader(FaceDataset(x_test, y_test, test_transform, cnn_loader), batch_size=512)
 
       return train_dataloader, test_dataloader
     else:
